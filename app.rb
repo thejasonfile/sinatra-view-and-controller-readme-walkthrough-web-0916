97e6f6896532c5bc03f3b22fb9782c5e0f604e6b
Jason Arnold <jason.arnold.00@gmail.com>
2016-10-14 09:23:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,11 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    @reversed_string = params["string"].reverse
     erb :reversed
   end
 
   get '/friends' do
     #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
